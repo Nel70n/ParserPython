8e52c37ef7a7034cafe89cfcd48986a2fed90310
simple <123@mail.ru>
2022-02-03 22:56:35 +0500

    main

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from ParserGoogle import ParseLink
 
-query = 'elon musk'
+flag = True
+print("*---------------------------------*")
+print("|   Pre testing parsing program   |")
+print("|                    ver. 0.0.1   |")
+print("| author:                         |")
+print("|         Trofimov Nikita         |")
+print("*---------------------------------*")
+while flag:
+    try: 
+        print('--> ', end="")
+        console_input = input()
+        if console_input == 'find file':
+            print("[*] Edit query:", end=" ")
+            query = input()
+            session = ParseLink(
+                query=query
+            )
+            session.CreateResponse()
+        elif console_input == 'exit':
+            flag = False
+        elif console_input == 'Help':
+            print("*-----------------------------------*")
+            print("| find file – find files from query |")
+            print("| exit – exit from program          |")
+            print("*-----------------------------------*")
 
-
-session = ParseLink(
-    query=query
-)
-
-session.CreateResponse()
+    except KeyboardInterrupt:
+        break
